62e7a43eca4caea232e770c565243959abc314e7
kindlich <kindlich@gmx.net>
2018-10-28 16:41:11 +0100

    remove mkdocs verbosity

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@
 echo "start";
 
 echo "Removing previous builds";
-rm -rfv /build/
+rm -frv ./build/*
 
 
 echo "Building english site";
-mkdocs build --verbose --clean --theme-dir ./mkdocs_windmill --site-dir ./build/en;
+mkdocs build --clean --theme-dir ./mkdocs_windmill --site-dir ./build/en;
 
 cd ./translations
 
 TRANS=./*
 
-for f in $TRANS; do cd $f echo "Processing folder $f"; SITEDIR="../../build/${f#"./"}"; echo $SITEDIR; mkdocs build --verbose --clean --theme-dir "../../mkdocs_windmill" --site-dir $SITEDIR; cd ..; done
+for f in $TRANS; do cd $f echo "Processing folder $f"; SITEDIR="../../build/${f#"./"}"; echo $SITEDIR; mkdocs build --clean --theme-dir "../../mkdocs_windmill" --site-dir $SITEDIR; cd ..; done
